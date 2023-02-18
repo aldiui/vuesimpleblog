e2e15d469b482af927097749f162e3d7a9ce7426
aldiui <aldijaya280902@gmail.com>
2023-02-18 21:08:13 +0700

    first commit

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <header
+            class="masthead"
+            style="background-image: url('/img/home-bg.jpg')"
+        >
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="site-heading">
+                            <h1>@duniacoding Home Blog</h1>
+                            <span class="subheading"
+                                >Build with
+                                <i class="fa-solid fa-heart text-danger"></i
+                            ></span>
+                            by @duniacoding
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <!-- Post -->
+        <main class="mb-4">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <p>
+                            Dunia Coding Blog adalah Blog yang dibuat untuk
+                            sharing informasi tentang teknologi dan tren
+                            terutama di bidang IT. Blog ini berisi banyak hal,
+                            mulai dari Tutorial, Template, Share Ilmu, sampai
+                            dengan Share pengalaman.
+                        </p>
+                        <p>
+                            Kedepannya Dunia Coding akan membuka Kontributor
+                            yang bisa berkontribusi untuk mengisi konten blog
+                            yang bisa dilihat oleh komunitas, sehingga
+                            memudahkan teman-teman yang lain
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
